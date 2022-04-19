f0ec552053b2e520c3e7e82ef5fa04bd8c7f8d63
Tofifiw <62210625+Tofifiw@users.noreply.github.com>
2022-04-19 15:08:38 +0200

    Add files via upload

--- src/main/scala/Calzone.scala
+++ src/main/scala/Calzone.scala
@@ -6,7 +6,7 @@ class Calzone extends PizzaType {
     size match {
       case Small => if ((quantity > 5) && discount.toUpperCase() == "FreePizza".toUpperCase()) quantity * 4.50f * 0.85f else quantity * 4.50f
       case Medium => if ((quantity > 4) && discount.toUpperCase() == "FreePizza".toUpperCase()) quantity * 7.25f * 0.85f else quantity * 7.25f
-      case Large => if ((quantity > 3) && discount.toUpperCase() == "FreePizza".toUpperCase) quantity * 10.25f * 0.75f else quantity * 10.25f
+      case Large => if ((quantity > 3) && discount.toUpperCase() == "FreePizza".toUpperCase()) quantity * 10.25f * 0.75f else quantity * 10.25f
     }
   }
 }
--- src/main/scala/RectanglePizza.scala
+++ src/main/scala/RectanglePizza.scala
@@ -4,9 +4,9 @@ class RectanglePizza extends PizzaType {
   def calculateProduct(quantity: Int, discount: String, size: PizzaSize): Float = {
 
     size match {
-      case Small => if ((quantity > 4) && discount.toUpperCase() == "FreePizza".toUpperCase) quantity * 5.80f * 0.85f else quantity * 5.80f
-      case Medium => if ((quantity > 3) && discount.toUpperCase() == "FreePizza".toUpperCase) quantity * 7.25f * 0.80f else quantity * 7.25f
-      case Large => if ((quantity > 2) && discount.toUpperCase() == "FreePizza".toUpperCase) quantity * 9.68f * 0.75f else quantity * 9.68f
+      case Small => if ((quantity > 4) && discount.toUpperCase() == "FreePizza".toUpperCase()) quantity * 5.80f * 0.85f else quantity * 5.80f
+      case Medium => if ((quantity > 3) && discount.toUpperCase() == "FreePizza".toUpperCase()) quantity * 7.25f * 0.80f else quantity * 7.25f
+      case Large => if ((quantity > 2) && discount.toUpperCase() == "FreePizza".toUpperCase()) quantity * 9.68f * 0.75f else quantity * 9.68f
     }
   }
 }
